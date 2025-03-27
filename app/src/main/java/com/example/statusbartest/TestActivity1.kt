a10eb45bf43d130d5168c0053b670d970127adae
Rajesh <rajeshkhuntia00@gmail.com>
2025-03-27 11:22:16 +0530

    commit Add KeyboardTopBlock and adjust CustomDigitKeyboard2 layout
    This commit introduces the `KeyboardTopBlock` composable, which displays date, time, and category information with icons and styled text within bordered rows. The `CustomDigitKeyboard2` composable's layout is also adjusted to include spacing between rows and accommodate the new top block, enhancing the visual structure and information presentation of the interface.

--- app/src/main/java/com/example/statusbartest/TestActivity1.kt
+++ app/src/main/java/com/example/statusbartest/TestActivity1.kt
@@ -6,25 +6,31 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.CheckBox
+import androidx.compose.material.icons.filled.Contrast
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.rounded.CalendarMonth
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -34,6 +40,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -68,8 +76,13 @@ fun Greeting2(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview2() {
     StatusBarTestTheme {
-        Column (Modifier.fillMaxSize().padding(12.dp)){
-            Spacer(modifier=Modifier.weight(1f))
+        Column(Modifier
+            .fillMaxSize()
+            .padding(12.dp)) {
+            Spacer(modifier = Modifier.weight(1f))
+            KeyboardTopBlock()
+            Spacer(modifier=Modifier.height(12.dp))
+
             CustomDigitKeyboard2()
         }
 
@@ -83,7 +96,7 @@ fun DigitKeyboard() {
 
 @Composable
 fun CustomDigitKeyboard2() {
-    val colorButton= Color.LightGray.copy(alpha = 0.2f)
+    val colorButton = Color.LightGray.copy(alpha = 0.2f)
     val buttons = listOf(
         listOf("1", "2", "3"),
         listOf("4", "5", "6"),
@@ -92,32 +105,51 @@ fun CustomDigitKeyboard2() {
 //        listOf("Clear")
     )
 
-    Column {
+    Column(
+//        Modifier.border(0.dp, Color.Black, RoundedCornerShape(12.dp)),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
         buttons.forEach { row ->
             Row(
                 modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
             ) {
                 row.forEach { label ->
-                    if(label != "Del"){
-                    Button(
-                        onClick = {
+                    if (label != "Del") {
+                        Button(
+                            onClick = {
 
-                        },colors= ButtonDefaults.buttonColors(
-                            containerColor = colorButton,
-                            contentColor = Color.White
-                        ) ,
-                        shape = RoundedCornerShape(12.dp), modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp).weight(1f).height(60.dp)
-                    ) {
-                        Text(label,fontSize = MaterialTheme.typography.headlineLarge.fontSize, color = Color.Black)
-                    }
-                    }
-                    else{
+                            },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = colorButton,
+                                contentColor = Color.White
+                            ),
+                            shape = RoundedCornerShape(12.dp),
+                            modifier = Modifier
+                                .padding(horizontal = 0.dp, vertical = 0.dp)
+                                .weight(1f)
+                                .height(60.dp)
+                        ) {
+                            Text(
+                                label,
+                                fontSize = MaterialTheme.typography.headlineLarge.fontSize,
+                                color = Color.Black
+                            )
+                        }
+                    } else {
 
                         IconButton(
                             onClick = { /* Handle click */ },
-                            modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp).weight(1f).height(60.dp).background(Color.Black.copy(alpha = 0.7f), shape = RoundedCornerShape(12.dp)))
-                         {
+                            modifier = Modifier
+                                .padding(horizontal = 0.dp, vertical = 0.dp)
+                                .weight(1f)
+                                .height(60.dp)
+                                .background(
+                                    Color.Black.copy(alpha = 0.7f),
+                                    shape = RoundedCornerShape(12.dp)
+                                )
+                        )
+                        {
                             Icon(
                                 imageVector = Icons.Default.Check,
                                 contentDescription = "Favorite",
@@ -131,3 +163,42 @@ fun CustomDigitKeyboard2() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun KeyboardTopBlock() {
+    val colorBorder= Color.Black.copy(alpha = 0.2f)
+    val borderWidth=1.5.dp
+    Row(Modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier
+                .padding(end = 12.dp)
+                .weight(1f)
+                .border(borderWidth, colorBorder, RoundedCornerShape(12.dp))
+                .padding(8.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.CalendarMonth,
+                contentDescription = "Favorite",
+                tint = Color.Black
+            )
+
+            Text("Today,27 Mar", fontSize = 18.sp, fontWeight = FontWeight.Medium)
+            Spacer(Modifier.weight(1f))
+            Text("10:13", fontSize = 18.sp, fontWeight = FontWeight.Medium)
+        }
+        Row(
+            modifier = Modifier
+                .border(borderWidth, colorBorder, RoundedCornerShape(12.dp))
+                .padding(8.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Contrast,
+                contentDescription = "Favorite",
+                tint = Color.Black
+            )
+            Spacer(Modifier.width(6.dp))
+            Text("Category", fontSize = 18.sp, fontWeight = FontWeight.Medium)
+        }
+    }
+
+}
